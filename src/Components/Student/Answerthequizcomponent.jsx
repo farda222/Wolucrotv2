bcfb225df63ecca6d9b60d98f3f83495b881c175
bli farda <134302929+farda222@users.noreply.github.com>
2024-04-17 20:13:28 +0700

    Update Answerthequizcomponent.jsx

--- src/Components/Student/Answerthequizcomponent.jsx
+++ src/Components/Student/Answerthequizcomponent.jsx
@@ -16,7 +16,7 @@ const Answerthequizcomponent = () => {
     },
     {
       id: 2,
-      question: "Which the most danger animal",
+      question: "Which the most dangerous animal",
       options: ["Elephant", "Tiger", "Lion", "Giraffe"],
       answer: "Lion",
     },
@@ -40,7 +40,7 @@ const Answerthequizcomponent = () => {
     },
     {
       id: 6,
-      question: "What is that?  ",
+      question: "What is that?",
       image: BlueWhaleImage,
       options: ["Mountain", "Forest", "Yard", "tree"],
       answer: "Mountain",
@@ -80,7 +80,7 @@ const Answerthequizcomponent = () => {
     return (
       <div className="font-Jakarta">
         <Navbar />
-        <div className="flex justify-center align-middle items-center mx-auto container gap-28 mt-10 ml-1 font-Jakarta lg:mt-28 2xl:mt-32 mb-4">
+        <div className="flex justify-center align-middle items-center mx-auto container gap-28 mt-10 ml-1 font-Jakarta lg:mt-12 2xl:mt-20 mb-4 lg:gap-96 lg:mb-24">
           <h1 className="text-black font-semibold text-2xl lg:text-3xl 2xl:ml-60">English XI PPLG 1</h1>
           <div className="relative flex-row -ml-4">
             <button className="flex items-center justify-center w-8 h-8 bg-white rounded-full focus:outline-none focus:bg-gray-300 lg:block lg:ml-6">
@@ -105,13 +105,15 @@ const Answerthequizcomponent = () => {
         </div>
         <div className="container mx-auto mt-12 flex justify-center px-2">
           <div className="bg-white rounded px-8 py-4 mb-4 flex items-center justify-center gap-6 border border-solid border-neutral-300">
-            <img src={AccountIcon} alt="Account Icon" className="w-11 h-11 -ml-3 rounded-full" />
-            <h1 className="text-lg font-semibold -ml-2">Alexander Agung</h1>
-            <p className="font-semibold mt-1 text-lg ml-4">{percentageScore}/100</p>
+            <img src={AccountIcon} alt="Account Icon" className="w-11 h-11 -ml- rounded-full " />
+            <h1 className="text-lg font-semibold -ml-2 lg:ml-5">Alexander Agung</h1>
+            <p className="font-semibold mt-1 text-lg ml-4 lg:ml-40">{percentageScore}/100</p>
           </div>
         </div>
-        <div className="flex justify-center items-center gap-28">
-          <p className="font-semibold">Ranking <span className="text-yellow-500"> 1 </span> / 36</p>
+        <div className="flex justify-center items-center gap-28 lg:mb-28 lg:gap-56 lg:mt-3">
+          <p className="font-semibold">
+            Ranking <span className="text-yellow-500"> 1 </span> / 36
+          </p>
           <p className="font-semibold">Duration 10.5m</p>
         </div>
         <div className="flex justify-center items-center ml-5 mt-10">
@@ -121,7 +123,7 @@ const Answerthequizcomponent = () => {
               Repeat
             </button>
             <button className="mt-3 px-[4.6rem] font-base py-2 border border-solid border-neutral-500 bg-white text-black rounded hover:bg-indigo-700 hover:border-indigo-600 hover:text-white transition-all flex mr-4">Save</button>
-            <button className="mt-3 px-[4.3rem] py-2 border border-solid border-neutral-500 bg-white text-black rounded hover:bg-indigo-700 transition-all flex">Home</button>
+            <button className="mt-3 px-[4.3rem] py-2 border border-solid border-neutral-500 bg-white text-black rounded hover:bg-indigo-700 hover:text-white transition-all flex">Home</button>
           </div>
         </div>
       </div>
@@ -134,7 +136,7 @@ const Answerthequizcomponent = () => {
     <div className="font-Jakarta">
       <Navbar />
       <div className="container mx-auto mt-12">
-        <div className="flex justify-center align-middle items-center mx-auto container gap-28 mt-1 ml-1 font-Jakarta lg:mt-28 2xl:mt-32 mb-4">
+        <div className="flex justify-center align-middle items-center mx-auto container gap-28 mt-1 ml-1 font-Jakarta lg:mt-1 2xl:mt-5 mb-4 lg:gap-96 2xl:justify-center 2xl:gap-[28rem] 2xl:-ml-32">
           <h1 className="text-black font-semibold text-2xl lg:text-3xl 2xl:ml-60">English XI PPLG 1</h1>
           <div className="relative flex-row -ml-4">
             <button className="flex items-center justify-center w-8 h-8 bg-white rounded-full focus:outline-none focus:bg-gray-300 lg:block lg:ml-6">
@@ -157,21 +159,23 @@ const Answerthequizcomponent = () => {
             </div>
           </div>
         </div>
-        <div className="ml-12">
-          <p className="h-full ml-10 absolute mt-[1.93rem]">
+        <div className="ml-12 lg:mb-10 2xl:mt-10">
+          <p className="h-full ml-10 absolute mt-[1.93rem] lg:ml-[27rem] 2xl:ml-[34rem]">
             {currentQuestion + 1}/{questions.length}
           </p>
-          <div className="bg-gray-200 h-2 rounded-full w-44 overflow-hidden mx-auto mb-10 mt-10">
+          <div className="bg-gray-200 h-2 rounded-full w-44 overflow-hidden mx-auto mb-10 mt-10 lg:w-64">
             <div className="bg-blue-500 h-2 rounded-full" style={{ width: `${progress}%`, transition: "width 0.5s ease-in-out" }}></div>
           </div>
         </div>
-        {questions[currentQuestion].image && <img src={questions[currentQuestion].image} alt="Question" className="mx-auto mb-4 rounded-lg" style={{ maxWidth: "100%", height: "auto" }} />}
-        <h2 className="text-xl font-semibold mb-4 justify-center flex">{questions[currentQuestion].question}</h2>
-        <div className="grid grid-row-2 gap-4 px-12">
+        {questions[currentQuestion].image && <img src={questions[currentQuestion].image} alt="Question" className="mx-auto mb-4 rounded-lg lg:w-96" style={{ maxWidth: "100%", height: "auto" }} />}
+        <h2 className="text-xl font-semibold mb-4 justify-center flex lg:justify-center lg:text-lg lg:mb-10 2xl:mt-10">{questions[currentQuestion].question}</h2>
+        <div className="grid grid-row-2 gap-4 px-12 lg:px-96">
           {questions[currentQuestion].options.map((option, index) => (
             <button
               key={index}
-              className={`py-2 rounded-md  option-button ${selectedOption !== null ? (option === questions[currentQuestion].answer ? "bg-indigo-600 text-white" : "bg-red-500 text-white") : "bg-gray-200"}`}
+              className={`py-2 rounded-md  option-button ${
+                selectedOption !== null ? (option === questions[currentQuestion].answer ? "bg-indigo-600 text-white" : "bg-red-500 text-white") : "bg-white border-2 border-neutral-300 border-solid"
+              }`}
               disabled={selectedOption !== null}
               onClick={() => handleOptionSelect(option)}>
               {option}
